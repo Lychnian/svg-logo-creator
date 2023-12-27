70403dc5326b302bea91b290e54158a027abf65d
unknown <helenvcolon@gmail.com>
2023-12-26 22:28:39 -0500

    Created folders lib and test, files shapes test and shapes and index with starter codes

---
+++ index.js
@@ -0,0 +1,4 @@
+// Import necessary modules
+const inquirer = require("inquirer"); // For user prompts
+const fs = require("fs"); // For file operations
+const { Circle, Square, Triangle } = require("./lib/shapes"); // Import shape classes from shapes.js
